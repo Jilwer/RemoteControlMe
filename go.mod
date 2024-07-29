c7ae52dad09b3cffdf44b66bdf0c8d2bc22bb72f
jilwer <165509268+Jilwer@users.noreply.github.com>
2024-07-29 12:01:22 -0400

    initial commit

---
+++ go.mod
@@ -0,0 +1,17 @@
+module remotecontrolme
+
+go 1.22.5
+
+require (
+	github.com/Jilwer/VRChatOscInput v0.0.0-20240729025900-c72461bb5e91 // indirect
+	github.com/google/go-cmp v0.5.7 // indirect
+	github.com/gorilla/securecookie v1.1.1 // indirect
+	github.com/gorilla/sessions v1.2.1 // indirect
+	github.com/hypebeast/go-osc v0.0.0-20220308234300-cec5a8a1e5f5 // indirect
+	github.com/jfyne/live v0.15.7 // indirect
+	github.com/klauspost/compress v1.15.1 // indirect
+	github.com/rs/xid v1.4.0 // indirect
+	golang.org/x/net v0.0.0-20220325170049-de3da57026de // indirect
+	golang.org/x/time v0.0.0-20220224211638-0e9765cccd65 // indirect
+	nhooyr.io/websocket v1.8.7 // indirect
+)
---
+++ main.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"context"
+	"html/template"
+	"log"
+	"net/http"
+	"time"
+
+	vrcinput "github.com/Jilwer/VRChatOscInput"
+	"github.com/jfyne/live"
+)
+
+const (
+	MoveForward = "up"
+	MoveBack    = "down"
+	MoveLeft    = "left"
+	MoveRight   = "right"
+	Jump        = "jump"
+	LookLeft    = "look-left"
+	LookRight   = "look-right"
+)
+
+func main() {
+	osc := vrcinput.NewOscClient(vrcinput.DefaultAddr, vrcinput.DefaultPort)
+
+	t, err := template.ParseFiles("root.html", "view.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	h := live.NewHandler(live.WithTemplateRenderer(t))
+
+	// Client side events.
+
+	// Jump event.
+	h.HandleEvent(Jump, func(ctx context.Context, s live.Socket, _ live.Params) (interface{}, error) {
+
+		osc.Jump()
+
+		return 1, nil
+	})
+
+	// Move forward event.
+	h.HandleEvent(MoveForward, func(ctx context.Context, s live.Socket, _ live.Params) (interface{}, error) {
+
+		osc.Move(vrcinput.MoveForward, true)
+		time.Sleep(1 * time.Second)
+		osc.Move(vrcinput.MoveForward, false)
+
+		return 1, nil
+	})
+
+	// Move back event.
+	h.HandleEvent(MoveBack, func(ctx context.Context, s live.Socket, _ live.Params) (interface{}, error) {
+
+		osc.Move(vrcinput.MoveBackward, true)
+		time.Sleep(1 * time.Second)
+		osc.Move(vrcinput.MoveBackward, false)
+
+		return 1, nil
+	})
+
+	// Move left event.
+	h.HandleEvent(MoveLeft, func(ctx context.Context, s live.Socket, _ live.Params) (interface{}, error) {
+
+		osc.Move(vrcinput.MoveLeft, true)
+		time.Sleep(1 * time.Second)
+		osc.Move(vrcinput.MoveLeft, false)
+
+		return 1, nil
+	})
+
+	// Move right event.
+	h.HandleEvent(MoveRight, func(ctx context.Context, s live.Socket, _ live.Params) (interface{}, error) {
+
+		osc.Move(vrcinput.MoveRight, true)
+		time.Sleep(1 * time.Second)
+		osc.Move(vrcinput.MoveRight, false)
+
+		return 1, nil
+	})
+
+	// Look left event.
+	h.HandleEvent(LookLeft, func(ctx context.Context, s live.Socket, _ live.Params) (interface{}, error) {
+
+		osc.Look(vrcinput.LookLeft, true)
+		time.Sleep(250 * time.Millisecond)
+		osc.Look(vrcinput.LookLeft, false)
+
+		return 1, nil
+	})
+
+	// Look right event.
+	h.HandleEvent(LookRight, func(ctx context.Context, s live.Socket, _ live.Params) (interface{}, error) {
+
+		osc.Look(vrcinput.LookRight, true)
+		time.Sleep(250 * time.Millisecond)
+		osc.Look(vrcinput.LookRight, false)
+
+		return 1, nil
+	})
+
+	// Run the server.
+	http.Handle("/", live.NewHttpHandler(live.NewCookieStore("session-name", []byte("weak-secret")), h))
+	http.Handle("/live.js", live.Javascript{})
+	http.Handle("/auto.js.map", live.JavascriptMap{})
+	http.ListenAndServe(":8080", nil)
+}
